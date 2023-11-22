91003963af99a7e8fb46a31f7e8f9e179ae83fb0
BrijrajRastogi <123160947+BrijrajRastogi@users.noreply.github.com>
2023-11-22 12:42:19 +0530

    Add files via upload

---
+++ Programs/Program.cs
@@ -0,0 +1,22 @@
+﻿//using the classes and methods from System namespace
+using System; 
+
+//creating a namespace called Syntax
+namespace Syntax 
+{
+    //creating a class named Program with internal access specifier
+    internal class Program 
+    {
+        //creating the Main method
+        static void Main(string[] args) 
+        {
+            //printing a statement using the WriteLine() method of Console class
+            Console.WriteLine("Hello World!");
+            //printing a statement using the WriteLine() method of Console class
+            Console.WriteLine("Welcome to C# program.");
+
+            //using the ReadLine() method of Console class to pause the console screen
+            Console.ReadLine();
+        }
+    }
+}
